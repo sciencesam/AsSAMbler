43fd82278bdbce686444bd9493f757ca7e633799
sciencesam13 <sciencesam13@gmail.com>
2017-03-10 10:09:42 -0500

    element by element approach

--- asm.c
+++ asm.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+int isReg(char *s);
+int isOpcode(char *s);
+int isLabel(char *s);
+int addToRegisterList(char *s);
+int fufillLabel();
+
 int main(int argc, char **argv){
 	//check arguments
 	if(argc < 2){
@@ -29,7 +35,105 @@ int main(int argc, char **argv){
 		printf("Cannot Output to %s", outputFilename);
 		return 1;
 	}
+	//define datatypes
+	struct {
+		char *name;
+		int line;
+	} labels[100];
+	int nLabel = 0;
+
+	struct {
+		char *name;
+		int location;
+	} registers[100];
+	int nReg = 0;
+
+
+	struct {
+		char *op;
+		char *label;
+		char *Dest;
+		char *Src;
+
+		int uin; //use input not
+		int we; //write enable
+
+		int opcode;
+		// Rd and Rs for 
+		int Rd;
+		int Rs;
+		//label address for branch
+		int Addr;
+
+		int hex; //final output
+
+	} operations[100];
+	int nOps = 0;
+
+
+	//collect line by line and format
+	char funExp[] = "\n\t\r ,"; //delimiters
+	char *token; //token for words
+	char line[100]; //temp storage for each line
+	char *operation[3]; //temp operation storage
+	int cCount = 2;
+	while(fgets(line, sizeof line, fp)!=NULL){
+		token = strtok(line, funExp);
+		while(token != NULL){
+
+			printf("|%s| ", token);
+
+			//check for label
+			if(isLabel(token)){
+				//add to label database
+			}
+			//check for opcode
+			if(isOpcode(token)){
+				fufillLabel(); //any un-enumerated labels point here
+
+			}
+			//check for Rd
+			if(cCount == 1 && isReg(token)){
+
+			}
+			//check for Rs or input
+			if(cCount == 0){
+				if(isReg(token)){
+
+				}
+				else {
+					//Function using Input
+				}
+
+			}
+
+
+
+			token = strtok(NULL, funExp);
+		}
+		printf("\n");
+	} //end of input
 
 
 	return 0;
+}
+
+int isReg(char *s){
+	//All registers are labeled with an 'R'
+	if(toupper(s[0]) == 'R') return 1;
+	else return 0;
+}
+int isOpcode(char *s){
+	//look through opcode dictionary for a match
+}
+int isLabel(char *s){
+	//The last character of a label is ':'
+	if(s[strlen(s)-1] == ":") return 1;
+	else return 0;
+}
+int addToRegisterList(char *s){
+
+}
+int fufillLabel(){
+
 }
