3820f6fd013a2f211b5e465e28e2348ca4372a0a
sciencesam13 <sciencesam13@gmail.com>
2017-03-15 15:04:02 -0400

    started python version

---
+++ asm.py
@@ -0,0 +1,15 @@
+# Sam Stone EECE@Binghamton 3/15/2017
+
+# I re-wrote the original asm in python
+
+import sys
+
+if len(sys.argv) < 2:
+	inputFileName = input('input file name?')
+	outputFileName = "out.sam"
+else:
+	inputFileName = str(sys.argv[1])
+	if len(sys.argv) < 4
+	outputFileName = str(sys.argv[2])
+
+inputFile = open("", 'r')
---
+++ pythonArgumentTest.py
@@ -0,0 +1,4 @@
+import sys
+
+print str(sys.argv)
+print str(sys.argv[1])
